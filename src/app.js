445581b935c18384d933682ad2a008bebeeebd92
mohamedgaafer <33942673+mohamedgaafer@users.noreply.github.com>
2020-12-04 14:39:03 +0200

    fire site-no database

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/fire', (req, res) => {
+    res.render('fire')
+})
+app.get('/safety', (req, res) => {
+    res.render('safety')
+})
+
+app.get('/alarm', (req, res) => {
+    res.render('alarm')
+})
+
+app.get('*', (req, res) => {
+    res.render('404')
+})
+
+app.listen(4000, () => {
+    console.log("Server is up on port 4000")
+})
+
+
+
+
+
